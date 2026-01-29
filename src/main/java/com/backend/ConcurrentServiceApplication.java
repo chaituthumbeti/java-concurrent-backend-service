7d5a3214ffde46fd11a3f7447fa295fc22c5f31b
chaituthumbeti <chaitanya998520@gmail.com>
2026-01-29 16:43:16 +0530

    Add application entry point and health endpoint

---
+++ src/main/java/com/backend/ConcurrentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentServiceApplication.class, args);
+    }
+}
